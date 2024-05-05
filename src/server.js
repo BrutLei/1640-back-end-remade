d6bff0fb18e5403e33928e637332aea9d5f60161
BrutLei <hoangphat4510@gmail.com>
2024-05-05 17:48:56 +0700

    .

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import topic_route from './routes/topicRoute.js';
 const port = 8080;
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://one640-back-end-remade.onrender.com/users/login', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
