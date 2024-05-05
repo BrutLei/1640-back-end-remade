25fec35250c6c5c386e9601ef20e7b5fe2183a52
BrutLei <hoangphat4510@gmail.com>
2024-05-05 17:28:12 +0700

    .

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,47 +4,7 @@ const prisma = new PrismaClient();
 
 async function main() {
   // Seed your database here
-
   // Example:
-  await prisma.faculties.create({
-    data: {
-      name: 'Information Technology',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      // Add more fields as necessary
-    },
-  });
-  await prisma.groups.createMany({
-    data: [
-      {
-        name: 'admin',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'student',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Marketing Manager',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Marketing Coordinator',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'guest',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ],
-  });
-
-  // Add more seeding logic for other models as needed
 }
 
 main()
